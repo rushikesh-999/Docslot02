43953952e8b53cbb15f84a03b1da68236476b47b
rushikesh-999 <rushikeshkadam0911@gmail.com>
2025-09-19 15:20:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ import adminRouter from './routes/adminRoute.js'
 import doctorRouter from './routes/doctorRoute.js'
 import userRouter from './routes/userRoute.js'
 
-
 //api config
 const app = express()
 const port = process.env.PORT || 4000
@@ -16,27 +15,27 @@ connectCloudinary()
 
 //middlewares
 const allowedOrigins = [
-  "http://localhost:5173",       // local frontend
-  "https://docslot02.vercel.app" // deployed frontend
-];
+  "http://localhost:5173",        // local frontend
+  "http://localhost:5174",        // local admin
+  "https://docslot02.vercel.app", // deployed frontend
+  "https://<your-admin-url>"      // deployed admin (replace with real URL later)
+]
 
 app.use(cors({
   origin: allowedOrigins,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
-}));
+}))
 
-app.use(express.json());
-
+app.use(express.json())
 
 //api endpoints
 app.use('/api/admin', adminRouter)
-app.use('/api/doctor',doctorRouter)
+app.use('/api/doctor', doctorRouter)
 app.use('/api/user', userRouter)
 
-
-app.get('/',(req,res)=>{
-    res.send('API WORKING ')
+app.get('/', (req, res) => {
+  res.send('API WORKING ')
 })
 
-app.listen(port, ()=> console.log("Server Started",port))
+app.listen(port, () => console.log("Server Started", port))
